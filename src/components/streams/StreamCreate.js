d0559f1288049ba496ed8c7255b31b3f7e79f8ec
iobaidu5 <i.obaidu5@gmail.com>
2020-11-23 18:13:16 -0800

    Add Redux Form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const StreamCreate = () => {
-
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component{
+   render () {
+    return (
+    <form>
+        <Field name="Title" />
+        <Field name="Description" />
+    </form>
+    );
+ }
 }
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
