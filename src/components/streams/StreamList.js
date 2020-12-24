272e3d075536921ef25f009aa6b9d906f8b904d0
iobaidu5 <i.obaidu5@gmail.com>
2020-12-23 16:01:39 -0800

    Stream Show

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -28,8 +28,9 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className=" large middle aligned camera retro icon"></i>
                         <div className="content">
-                      
-                            {stream.title}
+                            <Link to={`/streams/${stream.id}`}>
+                                {stream.title}
+                            </Link>
                         <div className="description">{stream.description}</div>
                         </div>
                 </div>
